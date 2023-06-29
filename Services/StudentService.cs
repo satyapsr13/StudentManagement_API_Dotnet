00c11b2a133ee47ac1f651a23d2e1eec4934782d
satyapsr13 <satyapsr13@gmail.com>
2023-06-30 01:16:29 +0530

    small changes

--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -1,6 +1,32 @@
-﻿namespace StudentManagement_API_Dotnet.Services
+﻿using StudentManagement_API_Dotnet.Models;
+
+namespace StudentManagement_API_Dotnet.Services
 {
-    public class StudentService
+    public class StudentService : IStudentService
     {
+        public Student CreateStudent(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetStudent(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Student> GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveStudent(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStudent(string id, Student student)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
