c2acd142cb231c40b0fb32ca1123662f837a74c6
satyapsr13 <satyapsr13@gmail.com>
2023-06-30 01:10:25 +0530

    service layer added

--- Models/StudentStoreDatabaseSettings.cs
+++ Models/StudentStoreDatabaseSettings.cs
@@ -3,8 +3,8 @@
     public class StudentStoreDatabaseSettings : IStudentStoreDatabaseSettings
     {
 
-        public string StudentCoursesCollectionName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string DatabaseName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string StudentCoursesCollectionName { get ; set ; }
+        public string ConnectionString { get ; set ; }
+        public string DatabaseName { get ; set ; }
     }
 }
